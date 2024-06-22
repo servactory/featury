0a1ce9c776dbfe8bd86e53b151caac5cdab8ff66
Anton <profox.rus@gmail.com>
2024-06-22 17:00:47 +0700

    Add options parameter to feature methods
    The FeatureLib and Base class methods have been refactored to accept variable options as parameters. This allows flexibility in passing optional parameters to these methods and might be beneficial for future feature enhancements or custom behavior injections.

--- examples/usual/example1/base.rb
+++ examples/usual/example1/base.rb
@@ -3,20 +3,20 @@
 module Usual
   module Example1
     class Base < Featury::Base
-      action :enabled? do |features:|
-        features.all? { |feature| FeatureLib.enabled?(feature) }
+      action :enabled? do |features:, **options|
+        features.all? { |feature| FeatureLib.enabled?(feature, **options) }
       end
 
-      action :disabled? do |features:|
-        features.any? { |feature| FeatureLib.disabled?(feature) }
+      action :disabled? do |features:, **options|
+        features.any? { |feature| FeatureLib.disabled?(feature, **options) }
       end
 
-      action :enable do |features:|
-        features.all? { |feature| FeatureLib.enable(feature) }
+      action :enable do |features:, **options|
+        features.all? { |feature| FeatureLib.enable(feature, **options) }
       end
 
-      action :disable do |features:|
-        features.all? { |feature| FeatureLib.disable(feature) }
+      action :disable do |features:, **options|
+        features.all? { |feature| FeatureLib.disable(feature, **options) }
       end
     end
   end
