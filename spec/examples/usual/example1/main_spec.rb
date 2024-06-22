3d1327e2cc1a32f9f35050f57f7e677e1cc56650
Anton <profox.rus@gmail.com>
2024-06-22 17:00:07 +0700

    Extend tests in main_spec.rb
    New tests were added to main_spec.rb under "describe #enabled?" These additional tests are designed to check if features in the FeatureLib are enabled, providing a more comprehensive coverage for each feature.

--- spec/examples/usual/example1/main_spec.rb
+++ spec/examples/usual/example1/main_spec.rb
@@ -11,7 +11,30 @@ RSpec.describe Usual::Example1::Main do
   describe "#enabled?" do
     subject(:perform) { described_class.enabled?(**arguments) }
 
+    before do
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_a).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_b).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_c).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_d_i).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_d_ii).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_d_iii).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_e_i).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_e_ii).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_e_ii).and_call_original
+      allow(FeatureLib).to receive(:enabled?).with(:example_1_e_iii).and_call_original
+    end
+
     it { expect(perform).to be(true) }
+
+    it { expect(FeatureLib.enabled?(:example_1_a)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_b)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_c)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_d_i)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_d_ii)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_d_iii)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_e_i)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_e_ii)).to be(true) }
+    it { expect(FeatureLib.enabled?(:example_1_e_iii)).to be(true) }
   end
 
   describe "#disabled?" do
