cfa90d31620a164298e13b45efeff4e6912a1635
Anton <profox.rus@gmail.com>
2024-06-22 19:16:48 +0700

    Improve base class example (#3)

--- examples/usual/example1/base.rb
+++ examples/usual/example1/base.rb
@@ -4,19 +4,19 @@ module Usual
   module Example1
     class Base < Featury::Base
       action :enabled? do |features:, **options|
-        features.all? { |feature| FeatureLib.enabled?(feature, **options) }
+        features.all? { |feature| FeatureLib.enabled?(feature, *options.values) }
       end
 
       action :disabled? do |features:, **options|
-        features.any? { |feature| FeatureLib.disabled?(feature, **options) }
+        features.any? { |feature| FeatureLib.disabled?(feature, *options.values) }
       end
 
       action :enable do |features:, **options|
-        features.all? { |feature| FeatureLib.enable(feature, **options) }
+        features.all? { |feature| FeatureLib.enable(feature, *options.values) }
       end
 
       action :disable do |features:, **options|
-        features.all? { |feature| FeatureLib.disable(feature, **options) }
+        features.all? { |feature| FeatureLib.disable(feature, *options.values) }
       end
     end
   end
