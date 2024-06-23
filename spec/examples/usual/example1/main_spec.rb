cd10aaaf8bfbaaf888f0ba11cd0a246acff04340
Anton <profox.rus@gmail.com>
2024-06-23 21:08:22 +0700

    Refactor test suite by using shared examples
    Instead of repeating the same tests for when the `with` method is used and not used, we create a shared example ("expected behavior") which gets reused. This not only cleans up the code by avoiding repetition but also makes it more maintainable.

--- spec/examples/usual/example1/main_spec.rb
+++ spec/examples/usual/example1/main_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe Usual::Example1::Main do
   let(:record) { Usual::Example1::Main::Record.new(id: "123") }
   let(:user) { Usual::Example1::Main::User.new(id: "456") }
 
-  context "when `with` method is used" do
+  shared_examples "expected behavior" do
     describe "#enabled?" do
-      subject(:perform) { described_class.with(**arguments).enabled? }
+      subject(:perform) { feature_class.enabled? }
 
       before do
         allow(FeatureLib).to receive(:enabled?).with(:example_1_a, user).and_call_original
@@ -42,7 +42,7 @@ RSpec.describe Usual::Example1::Main do
     end
 
     describe "#disabled?" do
-      subject(:perform) { described_class.with(**arguments).disabled? }
+      subject(:perform) { feature_class.disabled? }
 
       before do
         allow(FeatureLib).to receive(:disabled?).with(:example_1_a, user).and_call_original
@@ -71,7 +71,7 @@ RSpec.describe Usual::Example1::Main do
     end
 
     describe "#enable" do
-      subject(:perform) { described_class.with(**arguments).enable }
+      subject(:perform) { feature_class.enable }
 
       before do
         allow(FeatureLib).to receive(:enable).with(:example_1_a, user).and_call_original
@@ -100,7 +100,7 @@ RSpec.describe Usual::Example1::Main do
     end
 
     describe "#disable" do
-      subject(:perform) { described_class.with(**arguments).disable }
+      subject(:perform) { feature_class.disable }
 
       before do
         allow(FeatureLib).to receive(:disable).with(:example_1_a, user).and_call_original
@@ -129,121 +129,15 @@ RSpec.describe Usual::Example1::Main do
     end
   end
 
+  context "when `with` method is used" do
+    let(:feature_class) { described_class.with(**arguments) }
+
+    include_examples "expected behavior"
+  end
+
   context "when `with` method is not used" do
-    describe "#enabled?" do
-      subject(:perform) { described_class.enabled?(**arguments) }
+    let(:feature_class) { described_class }
 
-      before do
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_a, user).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_b, user).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_c, user).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_d_i).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_d_ii).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_d_iii).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_e_i).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:enabled?).with(:example_1_e_iii).and_call_original
-      end
-
-      it { expect(perform).to be(true) }
-
-      it { expect(FeatureLib.enabled?(:example_1_a, user)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_b, user)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_c, user)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_d_i)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_d_ii)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_d_iii)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_e_i)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_e_ii)).to be(true) }
-      it { expect(FeatureLib.enabled?(:example_1_e_iii)).to be(true) }
-    end
-
-    describe "#disabled?" do
-      subject(:perform) { described_class.disabled?(**arguments) }
-
-      before do
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_a, user).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_b, user).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_c, user).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_d_i).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_d_ii).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_d_iii).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_e_i).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:disabled?).with(:example_1_e_iii).and_call_original
-      end
-
-      it { expect(perform).to be(false) }
-
-      it { expect(FeatureLib.disabled?(:example_1_a, user)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_b, user)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_c, user)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_d_i)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_d_ii)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_d_iii)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_e_i)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_e_ii)).to be(false) }
-      it { expect(FeatureLib.disabled?(:example_1_e_iii)).to be(false) }
-    end
-
-    describe "#enable" do
-      subject(:perform) { described_class.enable(**arguments) }
-
-      before do
-        allow(FeatureLib).to receive(:enable).with(:example_1_a, user).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_b, user).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_c, user).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_d_i).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_d_ii).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_d_iii).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_e_i).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:enable).with(:example_1_e_iii).and_call_original
-      end
-
-      it { expect(perform).to be(true) }
-
-      it { expect(FeatureLib.enable(:example_1_a, user)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_b, user)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_c, user)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_d_i)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_d_ii)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_d_iii)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_e_i)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_e_ii)).to be(true) }
-      it { expect(FeatureLib.enable(:example_1_e_iii)).to be(true) }
-    end
-
-    describe "#disable" do
-      subject(:perform) { described_class.disable(**arguments) }
-
-      before do
-        allow(FeatureLib).to receive(:disable).with(:example_1_a, user).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_b, user).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_c, user).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_d_i).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_d_ii).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_d_iii).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_e_i).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_e_ii).and_call_original
-        allow(FeatureLib).to receive(:disable).with(:example_1_e_iii).and_call_original
-      end
-
-      it { expect(perform).to be(false) }
-
-      it { expect(FeatureLib.disable(:example_1_a, user)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_b, user)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_c, user)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_d_i)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_d_ii)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_d_iii)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_e_i)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_e_ii)).to be(false) }
-      it { expect(FeatureLib.disable(:example_1_e_iii)).to be(false) }
-    end
+    include_examples "expected behavior"
   end
 end
