32859b437f3a2a1ede5c06e3c4561e4d15ccdd65
Anton <profox.rus@gmail.com>
2024-11-08 23:48:14 +0700

    [08.11.2024] Add RSpec, RuboCop scripts and update Ruby/Rails versions (#7)

--- lib/featury/context/callable.rb
+++ lib/featury/context/callable.rb
@@ -30,12 +30,12 @@ module Featury
       def _call!(context, action, **arguments)
         context.send(
           :_call!,
-          action: action,
+          action:,
           incoming_arguments: arguments.symbolize_keys,
-          collection_of_resources: collection_of_resources,
-          collection_of_conditions: collection_of_conditions,
-          collection_of_features: collection_of_features,
-          collection_of_groups: collection_of_groups
+          collection_of_resources:,
+          collection_of_conditions:,
+          collection_of_features:,
+          collection_of_groups:
         )
       end
     end
