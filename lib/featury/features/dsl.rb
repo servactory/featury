a566b6a38d8c3fad4a893e52400b865b9ca5073f
Anton Sokolov <profox.rus@gmail.com>
2025-05-09 16:16:18 +0700

    Refactor string modification to use `delete_suffix`
    Replaced `sub` with `delete_suffix` for improved intent clarity and slightly better performance. This aligns with modern Ruby practices for manipulating string suffixes.

--- lib/featury/features/dsl.rb
+++ lib/featury/features/dsl.rb
@@ -40,7 +40,7 @@ module Featury
                 .gsub(/([a-z])(\d+)/, '\1_\2')
                 .gsub(/(\d+)([a-z])/, '\1_\2')
                 .tr("/", "_")
-                .sub("_feature", "")
+                .delete_suffix("_feature")
                 .to_sym
         end
 
